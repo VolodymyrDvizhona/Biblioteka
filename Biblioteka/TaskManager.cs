cc222aba63580c09ba2ee57d30352e17610677ab
VolodymyrDvizhona <mlunch12@gmail.com>
2024-06-11 00:18:46 +0200

    Add project files.

---
+++ Biblioteka/TaskManager.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using static BazaZstudentami.Baza;
+
+namespace BazaZstudentami
+{
+    public class Baza
+    {
+        public class Student
+        {
+            public string Imie;
+            public string Nazwisko;
+            public int NumerAlb;
+            public List<string> Przedmioty;
+            public double Nr;
+
+            public Student()
+            {
+                Przedmioty = new List<string>();
+            }
+        }
+
+        public class Kierunek
+        {
+            public string Nazwa;
+            public int CzasStudiow;
+        }
+
+        public class Grupa
+        {
+            public int Numer;
+            public double IloscStudentow;
+            public List<Student> Studenci;
+            public Kierunek KierunekGrupy;
+
+            public Grupa()
+            {
+                Studenci = new List<Student>();
+            }
+
+            public void WprowadzGrupe()
+            {
+                bool l = true;
+                while (l == true)
+                {
+                    try
+                    {
+                        Console.Write("Podaj numer grupy: ");
+                        Numer = Convert.ToInt32(Console.ReadLine());
+                        l = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                l = true;
+                while (l == true)
+                {
+                    try
+                    {
+                        KierunekGrupy = new Kierunek();
+                        Console.Write("Podaj nazwę kierunku: ");
+                        KierunekGrupy.Nazwa = Console.ReadLine();
+                        Console.Write($"Podaj na którym roku jest grupa {Numer}: ");
+                        KierunekGrupy.CzasStudiow = Convert.ToInt32(Console.ReadLine());
+                        l = false;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+                for (int i = 0; i < 30; i++)
+                {
+                    l = true;
+                    while (l == true)
+                    {
+                        try
+                        {
+                            Console.WriteLine($"\nWprowadzanie studenta {i + 1}:");
+                            Student student = new Student();
+                            student.Nr = i + 1;
+
+                            Console.Write("Podaj imię studenta: ");
+                            student.Imie = Console.ReadLine();
+
+                            Console.Write("Podaj nazwisko studenta: ");
+                            student.Nazwisko = Console.ReadLine();
+
+                            Console.Write("Podaj numer albumu studenta: ");
+                            student.NumerAlb = Convert.ToInt32(Console.ReadLine());
+
+                            Console.Write("Podaj liczbę ulubionych przedmiotów: ");
+                            int liczbaPrzedmiotow = Convert.ToInt32(Console.ReadLine());
+                            for (int j = 0; j < liczbaPrzedmiotow; j++)
+                            {
+                                Console.Write($"Podaj przedmiot {j + 1}: ");
+                                string lokalna = Console.ReadLine();
+                                student.Przedmioty.Add(lokalna);
+                            }
+
+                            Studenci.Add(student);
+                            IloscStudentow = Studenci.Count;
+                            l = false;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    Console.Write("\nCzy chcesz dodać kolejnego studenta(-tke)? (T/N): ");
+                    string odpowiedz = Console.ReadLine();
+                    if (odpowiedz.ToLower() != "t")
+                    {
+                        break;
+                    }
+                    l = true;
+                }
+            }
+
+            public void WypiszSzczegolyStudenta()
+            {
+                while (true)
+                {
+                    Console.Write("\nPodaj numer studenta: ");
+                    int index = Convert.ToInt32(Console.ReadLine());
+                    index--;
+                    Student student = Studenci[index];
+                    Console.WriteLine($"\nInformacja o studencie(-tce) (nr.{student.NumerAlb}):");
+                    Console.WriteLine($"Imię:{student.Imie}, Nazwisko: {student.Nazwisko}");
+                    if (student.Przedmioty.Count != 0)
+                    {
+                        Console.WriteLine("Ulubione przedmioty:");
+                        foreach (var przedmiot in student.Przedmioty)
+                        {
+                            Console.WriteLine($"{przedmiot}");
+                        }
+                    }
+                    else { Console.WriteLine("Brak ulubionych przedmiotów"); }
+                    break;
+                }
+            }
+
+            public void WypiszSzczegolyGrupy()
+            {
+                Console.Clear();
+                Console.WriteLine($"Numer grupy: {Numer}");
+                Console.WriteLine($"Ilość studentów: {IloscStudentow}");
+                Console.WriteLine($"Kierunek: {KierunekGrupy.Nazwa}");
+                Console.WriteLine($"Rok studiów: {KierunekGrupy.CzasStudiow}");
+                Console.WriteLine("\nLista studentów:");
+                foreach (var student in Studenci)
+                {
+                    Console.WriteLine($"{student.Nr}. {student.Imie} {student.Nazwisko}, Numer albumu: {student.NumerAlb}, Ilość ulubionych przedmiotów: {student.Przedmioty.Count}");
+                    Console.Write("\n");
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Program/Program.cs
@@ -0,0 +1,175 @@
+﻿using BazaZstudentami;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using static BazaZstudentami.Baza;
+
+namespace SterowaniaBaza
+{
+    class Program
+    {
+        static List<Baza.Grupa> bazaGrup = new List<Baza.Grupa>();
+        static List<Baza.Kierunek> bazaKierunkow = new List<Baza.Kierunek>();
+        static void Main(string[] args)
+        {
+            char c;
+            do
+            {
+                c = Menu();
+                switch (c)
+                {
+                    case 'a':
+                    case 'A':
+                        DodajGrupe();
+                        break;
+                    case 'b':
+                    case 'B':
+                        WypiszNumeryGrup();
+                        break;
+                    case 'c':
+                    case 'C':
+                        WyswietlSzczegolyGrupy();
+                        break;
+                    case 'd':
+                    case 'D':
+                        ZapiszBazeDoPliku();
+                        break;
+                    case 'k':
+                    case 'K':
+                        Console.Write("oniec programu :)\n\n");
+                        Environment.Exit(0);
+                        break;
+                }
+            }
+            while (!(c == 'k' || c == 'K'));
+        }
+
+        static char Menu()
+        {
+            Console.Clear();
+           
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("╔═════════════════════════════════════╗");
+            Console.WriteLine("║           Wybierz opcję:            ║");
+            Console.WriteLine("╠═════════════════════════════════════╣");
+            Console.ResetColor();
+            Console.WriteLine("║ A. Dodaj grupę                      ║");
+            Console.WriteLine("║ B. Wypisz wszystkie grupy           ║");
+            Console.WriteLine("║ C. Wyświetl informacje o grupie     ║");
+            Console.WriteLine("║ D. Zapisz bazę do pliku             ║");
+            Console.WriteLine("║ K. Wyjdź z programu                 ║");
+            Console.WriteLine("╚═════════════════════════════════════╝");
+            return Console.ReadKey().KeyChar;
+        }
+
+        static void DodajGrupe()
+        {
+            Console.Clear();
+            Baza.Grupa grupa = new Baza.Grupa();
+            grupa.WprowadzGrupe();
+            bazaGrup.Add(grupa);
+ 
+        }
+
+        static void WypiszNumeryGrup()
+        {
+            Console.Clear();
+            Console.WriteLine("Lista grup:");
+            int i = 1;
+           
+                foreach (var grupa in bazaGrup)
+                {
+
+                    Console.WriteLine(i + ". " + grupa.Numer);
+                    i++;
+                }
+                
+            
+            Console.WriteLine("Naciśnij dowolny klawisz aby kontynuować...");
+            Console.ReadKey();
+        }
+
+        static void WyswietlSzczegolyGrupy()
+        {
+            Console.Clear();
+            Console.WriteLine("Lista grup:");
+            int i = 1;
+            foreach (var grupa in bazaGrup)
+            {
+                Console.WriteLine(i + ". " + grupa.Numer);
+                i++;
+            }
+
+            Console.WriteLine("Podaj ID grupy do wyświetlenia informacji:");
+            int numerGrupy;
+
+            if (int.TryParse(Console.ReadLine(), out numerGrupy) && numerGrupy > 0 && numerGrupy <= bazaGrup.Count)
+            {
+                var grupa = bazaGrup [numerGrupy - 1];
+                Console.WriteLine($"Szczegóły płyty {grupa.Numer}:");
+                grupa.WypiszSzczegolyGrupy();
+
+                Console.Write("Chcesz wyswietlic informacje o konkretnym studencie(-tce)?(T/N): ");
+                char tn;
+                tn = Console.ReadKey().KeyChar;
+                if (tn == 'T' || tn == 't')
+                {
+                    var szczeg = bazaGrup[numerGrupy - 1];
+                    szczeg.WypiszSzczegolyStudenta();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Niepoprawny ID grupy.");
+                
+            }
+            
+
+            Console.WriteLine("\nNaciśnij dowolny klawisz aby kontynuować...");
+            Console.ReadKey();
+        }
+
+        static void ZapiszBazeDoPliku()
+        {
+            Console.Clear();
+            if (bazaGrup.Count != 0)
+            {
+                using (StreamWriter sw = new StreamWriter("baza.txt"))
+                {
+                    foreach (var grupa in bazaGrup)
+                    {
+
+                        sw.WriteLine("Numer grupy: " + grupa.Numer);
+                        sw.WriteLine("Ilość studentów: " + grupa.IloscStudentow);
+                        sw.WriteLine($"Kierunek: {grupa.KierunekGrupy.Nazwa}");
+                        sw.WriteLine($"Rok studiów: {grupa.KierunekGrupy.CzasStudiow}");
+                        sw.Write("\n");
+                        foreach (var student in grupa.Studenci)
+                        {
+                            sw.WriteLine($"Imię:{student.Imie}  Nazwisko:{student.Nazwisko}");
+                            sw.WriteLine("ID: " + student.NumerAlb);
+                            if (student.Przedmioty.Count != 0)
+                            {
+                                sw.WriteLine($"Ulubione przedmioty: ");
+                                sw.WriteLine(string.Join(", ", student.Przedmioty));
+                            }
+                            else { Console.WriteLine("Brak ulubionych przedmiotów"); }
+                            sw.Write("\n");
+                        }
+                    }
+                }
+                Console.WriteLine("Baza została zapisana do pliku.");
+                Console.WriteLine("Naciśnij dowolny klawisz aby kontynuować...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Brak danych w bazie.");
+                Console.WriteLine("Naciśnij dowolny klawisz aby kontynuować...");
+                Console.ReadKey();
+            }
+        }
+
+
+    }
+}
